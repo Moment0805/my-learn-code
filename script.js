174c1a302297c24874cf8919b7ea449ffaef42ab
Moment0805 <136906846+Moment0805@users.noreply.github.com>
2023-06-20 06:57:16 +0100

    New Files
    trying my best

---
+++ script.js
@@ -0,0 +1,12 @@
+alert('file linked')
+
+// letters,  numbers, under score, case sensitive 
+//Case sensitive  
+
+var test = 'This a test'
+var test = 'my number is lmao3' 
+
+var color = Array('23','12','32')
+alert(color[2])
+console.log(test
+);
